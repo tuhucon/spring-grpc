42c315d9a449a6605a5f9159f8d91e721c758952
Tuan Nguyen <tuan.nguyen1@tyme.com>
2023-10-10 11:01:59 +0700

    change default cached executor --> newVirtualThreadPerTeask Executor

--- server/src/main/java/org/example/Main.java
+++ server/src/main/java/org/example/Main.java
@@ -1,10 +1,21 @@
 package org.example;
 
+import net.devh.boot.grpc.server.serverfactory.GrpcServerConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class Main {
+
+    @Bean
+    public GrpcServerConfigurer getGrpcServerConfigurer() {
+        return serverBuilder -> {
+            serverBuilder.executor(Executors.newVirtualThreadPerTaskExecutor());
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
